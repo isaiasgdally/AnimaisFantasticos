4f9b94caba538ed7c2f0e9492b8f541730a7324a
Isaias Dally <isaiasgdally@gmail.com>
2025-09-08 11:39:29 -0300

    scroll suave links internos

--- script.js
+++ script.js
@@ -23,22 +23,41 @@ tabNav();
 
 //ACCORDION LIST
 function accordionNav(){
-
-
 const accordionList = document.querySelectorAll('.js-accordion dt');
+
 accordionList[0].classList.add('ativo');
 accordionList[0].nextElementSibling.classList.add('ativo');
 
 if(accordionList.length){
-
 function activeAccordion(){
     this.classList.toggle('ativo');
     this.nextElementSibling.classList.toggle('ativo');
 };
-    
+
 accordionList.forEach((item) =>{
     item.addEventListener('click', activeAccordion);
 })
 };
 };
 accordionNav();
+
+//SCROLL SUAVE(links internos)
+function linksIntScroll(){
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+    function scrollToSections(event){
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href)
+
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        })
+    };
+
+    linksInternos.forEach((links) =>{
+        links.addEventListener('click', scrollToSections)
+    });
+}
+linksIntScroll();
