e7753d4c5d2cf6fd5bb66d4b30f015a0ff500ebc
Isaias Dally <isaiasgdally@gmail.com>
2025-09-08 11:05:40 -0300

    accordion list

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// NAVEGAÇÃO POR TAB
+function tabNav(){
 const tabMenu = document.querySelectorAll('.js-tabmenu li');
 const tabContent = document.querySelectorAll('.js-tabcontent section');
 
@@ -8,11 +10,35 @@ function activeTab(index) {
         section.classList.remove('ativo');
     });
     tabContent[index].classList.add('ativo');
-}
+};
 
 tabMenu.forEach((itemMenu, index) => {
     itemMenu.addEventListener('click', () => {
         activeTab(index);
     });
 });
-}
+};
+};
+tabNav();
+
+//ACCORDION LIST
+function accordionNav(){
+
+
+const accordionList = document.querySelectorAll('.js-accordion dt');
+accordionList[0].classList.add('ativo');
+accordionList[0].nextElementSibling.classList.add('ativo');
+
+if(accordionList.length){
+
+function activeAccordion(){
+    this.classList.toggle('ativo');
+    this.nextElementSibling.classList.toggle('ativo');
+};
+    
+accordionList.forEach((item) =>{
+    item.addEventListener('click', activeAccordion);
+})
+};
+};
+accordionNav();
