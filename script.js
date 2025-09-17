0e477297aa0d0077cb9b65d8a2e4f902b2f5fc94
Isaias Dally <isaiasgdally@gmail.com>
2025-09-17 10:15:17 -0300

    reorganizacao

--- script.js
+++ script.js
@@ -1,88 +1,84 @@
 // NAVEGAÇÃO POR TAB
-function tabNav(){
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
+function tabNav() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
 
+  if (tabMenu.length && tabContent.length) {
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove("ativo");
+      });
+      tabContent[index].classList.add("ativo");
+    }
 
-if(tabMenu.length && tabContent.length){
-function activeTab(index) {
-tabContent.forEach((section) => {
-        section.classList.remove('ativo');
-});
-tabContent[index].classList.add('ativo');
-};
-
-tabMenu.forEach((itemMenu, index) => {
-itemMenu.addEventListener('click', () => {
-    activeTab(index);
-});
-});
-};
-};
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => {
+        activeTab(index);
+      });
+    });
+  }
+}
 tabNav();
 
 //ACCORDION LIST
-function accordionNav(){
-const accordionList = document.querySelectorAll('.js-accordion dt');
+function accordionNav() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
 
-accordionList[0].classList.add('ativo');
-accordionList[0].nextElementSibling.classList.add('ativo');
+  accordionList[0].classList.add("ativo");
+  accordionList[0].nextElementSibling.classList.add("ativo");
 
-if(accordionList.length){
-function activeAccordion(){
-    this.classList.toggle('ativo');
-    this.nextElementSibling.classList.toggle('ativo');
-};
+  if (accordionList.length) {
+    function activeAccordion() {
+      this.classList.toggle("ativo");
+      this.nextElementSibling.classList.toggle("ativo");
+    }
 
-accordionList.forEach((item) =>{
-    item.addEventListener('click', activeAccordion);
-})
-};
-};
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
 accordionNav();
 
 //SCROLL SUAVE(links internos)
-function linksIntScroll(){
-const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+function linksIntScroll() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
 
-function scrollToSections(event){
-     event.preventDefault();
-    const href = event.currentTarget.getAttribute('href');
-    const section = document.querySelector(href)
+  function scrollToSections(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
 
-section.scrollIntoView({
-    behavior: 'smooth',
-    block: 'start',
-    })
-};
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
 
-linksInternos.forEach((links) =>{
-    links.addEventListener('click', scrollToSections)
-});
+  linksInternos.forEach((links) => {
+    links.addEventListener("click", scrollToSections);
+  });
 }
 linksIntScroll();
 
-
 //ANIMAÇÃO AO SCROLL
 function scrollAnimation() {
-  const sections = document.querySelectorAll('.js-scroll');
-  if(sections.length) {
+  const sections = document.querySelectorAll(".js-scroll");
+  if (sections.length) {
     const windowMetade = window.innerHeight * 0.6;
 
     function animaScroll() {
       sections.forEach((section) => {
         const sectionTop = section.getBoundingClientRect().top;
-        const isSectionVisible = (sectionTop - windowMetade) < 0;
-        if(isSectionVisible)
-          section.classList.add('ativo');
-        else 
-          section.classList.remove('ativo');
-      })
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) section.classList.add("ativo");
+        else section.classList.remove("ativo");
+      });
     }
 
     animaScroll();
 
-    window.addEventListener('scroll', animaScroll);
+    window.addEventListener("scroll", animaScroll);
   }
 }
 scrollAnimation();
